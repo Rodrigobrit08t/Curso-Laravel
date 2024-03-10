d71513f589bfd2d5c33a3fbefaa2333359375ce7
Rodrigo Brito <118493422+Rodrigobrit08t@users.noreply.github.com>
2024-03-10 10:20:50 -0300

    inicio do projeto, rotas, parametros, redirect, view e rotas

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/empresa', function(){
+    return view('site/empresa');
+});
+
+Route::any('/any', function(){
+    return "Permite todo tipo de acesso http (put, delete, get, post)";
+});
+    
+
+Route::match(['get', 'post'], '/match', function(){
+    return "Permite apenas acessos definidos";
+});
+
+Route::get('/produto/{id}/{cat}', function($id, $cat = ''){
+    return "O id do produto é: ".$id. "<br>"."A categoria é: " .$cat;
+});
+
+Route::redirect('/sobre', '/empresa');
+Route::view('/empresa', 'site/empresa');
